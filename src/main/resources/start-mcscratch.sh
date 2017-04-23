16411e00a150462d644a6ef4704019bf634b1415
Paulo Carvalho <plcarvalho@gmail.com>
2017-04-23 15:23:32 +0100

    Added mcpi-scratch to startup script

--- src/main/resources/start-mcscratch.sh
+++ src/main/resources/start-mcscratch.sh
@@ -1,1 +1,6 @@
-java -Xmx1024M -Xms1024M -jar spigot*.jar nogui
+#!/bin/bash
+if [ "$SERVER_TYPE" == "MCPI" ];then
+  cd mcpi-scratch; python mcpi-scratch.py
+else 
+  java -Xmx1024M -Xms1024M -jar spigot*.jar nogui
+fi
