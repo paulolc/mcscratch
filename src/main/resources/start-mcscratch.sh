f3753d3bd0d6b7e1f7969e189bde4b3559a83bb0
Paulo Carvalho <plcarvalho@gmail.com>
2017-04-25 07:24:24 -0700

    Added glowstone as default build profile. Updated MCPI-scratch and is not started in the background

--- src/main/resources/start-mcscratch.sh
+++ src/main/resources/start-mcscratch.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if [ "$SERVER_TYPE" == "MCPI" ];then
-  cd mcpi-scratch; python mcpi-scratch.py
-else 
-  java -Xmx1024M -Xms1024M -jar spigot*.jar nogui
-fi
+( cd mcpi-scratch; python mcpi-scratch.py ) & 
+java -Xmx1024M -Xms1024M -jar *.jar nogui
